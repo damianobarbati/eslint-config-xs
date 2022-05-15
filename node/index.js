1c32515d5532b85ad5ce701342ce360e356a816c
damiano.barbati <damiano.barbati@gmail.com>
2022-05-15 16:16:09 +0200

    wip

--- node/index.js
+++ node/index.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
+const path = require('path');
 
 const package_name = process.env.npm_package_name;
-const babelrc = package_name === 'eslint-config-xs' ? './.babelrc' : 'node_modules/eslint-config-xs/react/.babelrc';
-const babelOptions = JSON.parse(fs.readFileSync(babelrc, 'utf8'));
+// const babelrc = package_name === 'eslint-config-xs' ? './.babelrc' : 'node_modules/eslint-config-xs/react/.babelrc';
+// const babelOptions = JSON.parse(fs.readFileSync(babelrc, 'utf8'));
+const tsconfig = package_name === 'eslint-config-xs' ? './tsconfig.json' : 'node_modules/eslint-config-xs/node/tsconfig.json';
+// const tsconfigOptions = JSON.parse(fs.readFileSync(tsconfig, 'utf8'));
 
 module.exports = {
   env: {
@@ -11,18 +14,15 @@ module.exports = {
     jest: true,
     node: true
   },
-  parser: '@babel/eslint-parser',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2022,
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-    requireConfigFile: false,
-    babelOptions,
+    files: ['*.js', '*.ts'],
+    project: './tsconfig.json',
   },
-  plugins: ['prettier', 'import', 'jest'],
-  extends: ['eslint:recommended', 'prettier', 'plugin:jest/recommended'],
+  plugins: ['@typescript-eslint', 'prettier', 'import', 'jest'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking', 'prettier', 'plugin:jest/recommended'],
   rules: {
     'no-console': 'off',
     'max-lines-per-function': ['error', 200],
--- node/index.spec.js
+++ node/index.spec.js
@@ -1,14 +1,29 @@
 const { strict: assert } = require('assert');
 const { ESLint } = require('eslint');
 
+// (async () => {
+//   const eslint = new ESLint({
+//     overrideConfigFile: './index.js',
+//   });
+//
+//   const code = `const foo = "bar";`;
+//   const [result] = await eslint.lintText(code);
+//   const rules = result.messages.map(m => m.ruleId);
+//
+//   assert.deepEqual(rules.includes('no-unused-vars'), true);
+//   assert.deepEqual(rules.includes('prettier/prettier'), true);
+// })();
+
 (async () => {
   const eslint = new ESLint({
     overrideConfigFile: './index.js',
   });
 
-  const code = `const foo = "bar";`;
-  const [result] = await eslint.lintText(code);
+  const code = `const foo: string = 123;`;
+
+  const [result] = await eslint.lintText(code, { filePath: './blabla.ts' });
   const rules = result.messages.map(m => m.ruleId);
+  console.log(result)
 
   assert.deepEqual(rules.includes('no-unused-vars'), true);
   assert.deepEqual(rules.includes('prettier/prettier'), true);
